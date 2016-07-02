eeab0e4d3cda93a78218081d96f9f6d2ba4528ef
honwen.chan <honwen.chan@gmail.com>
2016-07-03 02:57:38 +0800

    Add TestCase for gui-config.json

---
+++ config_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestConfigJson(t *testing.T) {
+	config, err := ParseConfig("testdata/test-gui-config.json")
+	if err != nil {
+		t.Fatal("error parsing config.json:", err)
+	}
+
+	if len(config.Servers) != 3 {
+		t.Error("wrong nums of Servers from config")
+	}
+
+	for i, server := range config.Servers {
+		if server.Server != fmt.Sprintf("ss%02d.test.com", i+1) {
+			t.Error("wrong Server of Server", i+1)
+		}
+		if server.Password != fmt.Sprintf("PPAASS%02d", i+1) {
+			t.Error("wrong Password of Server", i+1)
+		}
+		if server.ServerPort != fmt.Sprintf("%d", i+50001) {
+			t.Error("wrong ServerPort of Server", i+1)
+		}
+		if server.Method != "rc4-md5" {
+			t.Error("wrong Method of Server", i+1)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -6,14 +6,17 @@ import (
 )
 
 var (
-	ErrNoGuiConfigs = errors.New(`NO gui-config*.json`)
+	ErrNoGuiConfigs    = errors.New(`NO gui-config*.json`)
+	ErrWrongGuiConfigs = errors.New(`Wrong gui-config*.json`)
 
 	guiConfig *Config
 )
 
 func main() {
 	if files := getFileList(getCurrDir(), isGuiConfig); files != nil {
-		guiConfig, _ = ParseConfig(files[0])
+		if guiConfig, _ = ParseConfig(files[0]); guiConfig == nil {
+			panic(ErrWrongGuiConfigs)
+		}
 	} else {
 		panic(ErrNoGuiConfigs)
 	}
